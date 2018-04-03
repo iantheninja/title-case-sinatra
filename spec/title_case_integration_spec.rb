d364b042058fb8440d70738a1ee2ef6219b0f5e0
Ian Munene <munene.ian@live.com>
2018-04-03 19:03:20 +0300

    removed debugging line in integration spec

--- spec/title_case_integration_spec.rb
+++ spec/title_case_integration_spec.rb
@@ -4,7 +4,6 @@ Capybara.app = Sinatra::Application
 
 describe('the title case path', {:type => :feature}) do
   it "processes the user entry and returns it title cased" do
-    save_and_open_page
     visit('/')
     fill_in('title', :with => 'green eggs and ham')
     click_button('Send')
